2c5c77b0b3bf480803431bf6b777fa10431a2c0e
Thanh Thủy <69446347+ThanhThuy0411@users.noreply.github.com>
2022-03-17 00:21:55 +0700

    Initial commit

---
+++ src/main/java/GJS/com/junkShop/JunkShopApplication.java
@@ -0,0 +1,13 @@
+package GJS.com.junkShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunkShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunkShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GJS/com/junkShop/JunkShopApplicationTests.java
@@ -0,0 +1,13 @@
+package GJS.com.junkShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunkShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
